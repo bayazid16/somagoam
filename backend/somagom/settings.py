2ae62b882949b0b8b726f2440888ffc69503641c
bayazid16 <20230557029@juniv.edu>
2026-04-27 13:57:06 +0600

    aws storage settings and storage backends

--- backend/somagom/settings.py
+++ backend/somagom/settings.py
@@ -525,10 +525,12 @@ else:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_ENDPOINT_URL = f'https://sgp1.digitaloceanspaces.com' 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.sgp1.digitaloceanspaces.com'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.sgp1.digitaloceanspaces.com'
+    AWS_S3_CUSTOM_DOMAIN = os.getenv("DO_SPACES_CDN")
     
     AWS_S3_FILE_OVERWRITE = False
     AWS_QUERYSTRING_AUTH = False
+    AWS_S3_ADDRESSING_STYLE = "virtual"
     AWS_DEFAULT_ACL = 'public-read'
     
     
