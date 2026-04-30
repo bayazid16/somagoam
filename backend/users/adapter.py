9e14c5ad7f4f403704db2494cd136550dea718c6
bayazid16 <20230557029@juniv.edu>
2026-04-30 21:51:56 +0600

    send mail html setup

--- backend/users/adapter.py
+++ backend/users/adapter.py
@@ -1,7 +1,35 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from rest_framework.exceptions import ValidationError
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+
+
+    def send_mail(self, template_prefix, email, context):
+        subject = render_to_string(
+            f"{template_prefix}_subject.txt", context
+        ).strip()
+
+        text_body = render_to_string(
+            f"{template_prefix}_message.txt", context
+        )
+
+        html_body = render_to_string(
+            f"{template_prefix}_message.html", context
+        )
+
+        msg = EmailMultiAlternatives(
+            subject=subject,
+            body=text_body,
+            from_email=self.get_from_email(),
+            to=[email],
+        )
+        msg.attach_alternative(html_body, "text/html")
+        msg.send()
+
+    
     def validate_unique_email(self, email):
         if self.is_email_taken(email):
             raise ValidationError(
