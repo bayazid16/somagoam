ce1c921da3cc0af696a75be8054765fe56cfc7a4
bayazid16 <20230557029@juniv.edu>
2026-06-13 04:40:28 +0600

    seller view

--- backend/somagom/settings.py
+++ backend/somagom/settings.py
@@ -248,7 +248,7 @@ REST_FRAMEWORK={
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':10,
+    'PAGE_SIZE':12,
 
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',    #rate limit
