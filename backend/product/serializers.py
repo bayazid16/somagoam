779e79a73830040fa03f1752f609d6a9c9eed8f9
bayazid16 <20230557029@juniv.edu>
2026-07-28 19:13:16 +0600

    add stock and average

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -36,7 +36,7 @@ class ProductSerializer(serializers.ModelSerializer):
  
     class Meta:
         model=Product
-        fields=['id','name','price','category','category_name','slug','image','description','seller_name','seller_slug']
+        fields=['id','name','price','category','category_name','slug','image','description','seller_name','seller_slug','stock','average_rating']
 
 
 
